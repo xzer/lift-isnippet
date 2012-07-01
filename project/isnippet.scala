86ec235f1a6cbe9bcae624419ca1abfb8cc06cdb
xzer <xiaozhu@gmail.com>
2012-07-01 13:04:08 +0900

    add web-plugin to sbt
    It seems that web-plugin cannot work well for sub projects, but temporarily it solved the most problems...

--- project/isnippet.scala
+++ project/isnippet.scala
@@ -7,7 +7,7 @@ object HelloBuild extends Build {
     organization := "net.xzer",
     version      := "0.1",
     scalaVersion := "2.9.1"
-  )
+  ) ++ Seq(com.github.siasia.WebPlugin.webSettings: _*)
 
 	val dependencies = Seq(libraryDependencies ++= {
        val liftVersion = "2.4"
@@ -18,7 +18,8 @@ object HelloBuild extends Build {
                "junit" % "junit" % "4.7" % "test",
                "ch.qos.logback" % "logback-classic" % "0.9.26",
                "org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
-               "com.h2database" % "h2" % "1.2.147"
+               "com.h2database" % "h2" % "1.2.147",
+			   "org.mortbay.jetty" % "jetty" % "6.1.22" % "container"
        )
 	})
 
